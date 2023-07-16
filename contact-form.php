fc4536221e2103801926a23ea5cb0f47984136fb
verzakra <136198193+verzakra@users.noreply.github.com>
2023-07-16 10:54:38 +0200

    Stran ima sedaj delujoč Kontaktni form. V html-ju je javascript, ki skrije del form-a ko je obkljukan checkbox 'Fizična oseba'. PHP še ne prepoznava checkbox-a

--- contact-form.php
+++ contact-form.php
@@ -2,8 +2,23 @@
 
 $name = $_POST["ime-priimek"];
 $email = $_POST["email"];
+$checkBox = $_POST["fiz-oseba"];
+$podjejte = $_POST["podjetje"];
+$davcna = "SI".$_POST["davcna"];
 $besedilo = $_POST["besedilo"];
-$recieverMail = "kristjan.srdoc@gmail.com";
-$header = "Pošiljatelj: " . $name . $email;
 
-mail($recieverMail, "", $besedilo, $header);
+
+$recieverMail = "kospendavitan@gmail.com";
+
+if((isset($checkBox)) || empty($checkBox)){
+    $zadeva = "Pošiljatelj: " . $name ." <". $email .">\r\n";
+}
+else{
+    $zadeva = "Pošiljatelj: " . $name ." <". $email .">" . " Podjetje: " . $podjejte . " Davčna št.: " . $davcna;
+}
+
+
+mail($recieverMail, $zadeva, $besedilo);
+
+echo "Sporočilo poslano!";
+?>
